9a10c935c59b8847eb46478dce13b819119b78d2
MarwenCode <marwen_jdidi@yahoo.com>
2023-05-01 17:32:37 +0200

    inherutance & polymorphism

--- OpenClassRoom/src/OC.java
+++ OpenClassRoom/src/OC.java
@@ -1,4 +1,10 @@
+import javafx.scene.control.TextField;
+
 public class OC {
+
+    // enum Direction {
+    //     north, east, south, west;
+    // }
 
     public static void main(String[] args) {
 
@@ -6,32 +12,131 @@ public class OC {
         // Bonjour("Mike");
 
         // for (int i = 0; i < 10; i++) {
-        //     Bonjour("Mike");
+        // Bonjour("Mike");
         // }
 
-        int[] myArray = new int[]{7,2,4};
-        for (int i=0; i<myArray.length; i++) {
-            System.out.println(myArray[i]);
-        }
+        // int[] myArray = new int[]{7,2,4};
+        // for (int i=0; i<myArray.length; i++) {
+        // System.out.println(myArray[i]);
+        // }
 
-        int n = 10;
-        int numTrees = numberOftrees(n);
-        System.out.println("Total Number of trees printed: " + numTrees);
+        // int n = 10;
+        // int numTrees = numberOftrees(n);
+        // System.out.println("Total Number of trees printed: " + numTrees);
 
-
-        int i = 5;
-        boolean sunIsUp = theSunisUp(i);
-        System.out.println(sunIsUp);
+        // int i = 5;
+        // boolean sunIsUp = theSunisUp(i);
+        // System.out.println(sunIsUp);
 
         // int i = 0;
         // while (i < 20 ) {
-        //     i = randomNumber();
-        //     print(i);
+        // i = randomNumber();
+        // print(i);
         // }
+
+        // handle the conditions :
+        // int number = 1;
+        // if (number == 1) {
+        //     console("if 1");
+
+        // } else if (number == 2) {
+        //     console("if 2 is true");
+
+        // } else {
+        //     console("different number");
+        // }
+
+        // switch (number) {
+        //     case 1:
+        //         console("switch 1");
+        //         break;
+
+        //     default:
+        //         console("default");
+
+        // };
+
+        // second example to handle the conditions
+        String[] arg = {"this is the text "};
+
+        if (arg.length == 1) {
+            console2(arg[0]);
+        } else if (arg.length == 2) {
+            console2(arg[0] + "-" + arg[1]);
+        } else if (arg.length == 3) {
+            console2(arg[0] + "-" + arg[1] + "-" + arg[2]);
+        } else {
+            console2("this text contain more than 3 letters");
+        }
+
+
+
+       // switch directions
+       String direction = "north";
+     switch(direction) {
+        case "north":
+        System.out.println("You are heading north");
+        break;
+    case "east":
+        System.out.println("You are heading east");
+        break;
+    case "south":
+        System.out.println("You are heading south");
+        break;
+    case "west":
+        System.out.println("You are heading west");
+        break;
+        
+
+     }
+
+
+
+
     }
 
+
+    //directions to switch cases :
+    static void Direction(String text) {
+       
+        System.out.println(text);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // static void Bonjour(String name) {
-    //     System.out.println("Hello " + name + " ! . How are you ?");
+    // System.out.println("Hello " + name + " ! . How are you ?");
     // }
 
     static void print(int nombre) {
@@ -51,31 +156,34 @@ public class OC {
         return numbersOfTrees;
     }
 
-
-    static  boolean theSunisUp(int n ) {
+    static boolean theSunisUp(int n) {
         while (n >= 0) {
             System.out.println("the sun is up so wake up");
-            return true;
-        } 
+
+        }
 
         System.out.println("still night");
         return false;
-    
-
 
     }
-   
 
-
-
-
-}
-
-class Exemple {
-
-    static String text = "Hello, it's me!";
-
-    static void prinText() {
+    // handle the conditions and else if else, switch :
+    static void console(String text) {
         System.out.println(text);
     }
+
+   // handle 3 conditions
+   static void console2(String text) {
+    System.out.print(text);
 }
+
+}
+
+// class Exemple {
+
+// static String text = "Hello, it's me!";
+
+// static void prinText() {
+// System.out.println(text);
+// }
+// }
---
+++ OpenClassRoom/src/Poo.java
@@ -0,0 +1,79 @@
+import javafx.concurrent.WorkerStateEvent;
+
+public class Poo {
+
+    public static void main(String[] args){
+        Brand appleBrand = new Brand("Apple");
+        // System.out.println(apple.name);
+
+        Mobile appleMobile = new Mobile(18,"applex11", appleBrand);
+           System.out.println(appleMobile.name);
+        System.out.println(appleMobile.pixels);
+        System.out.println(appleMobile.brand.name);
+
+
+        Car car = new Car();
+        car.start("car : ");
+
+
+    }
+
+
+}
+
+    class Mobile {
+        int pixels;
+        String name;
+        Brand brand;
+
+
+        public Mobile(int pixels, String name, Brand brand) {
+            this.pixels = pixels;
+            this.name = name;
+            this.brand = brand;
+        }
+    }
+
+
+    class Brand {
+        String name;
+
+        public Brand(String name) {
+            this.name = name;
+        }
+    }
+
+    class Vehicule {
+        void start(String args) {
+            System.out.println(args + "sound of the vehicule");
+        }
+
+    }
+
+    //Inheritance && polymorphism  
+
+    class Car extends Vehicule {
+
+        @Override
+        void start(String args) {
+            super.start(args);
+            workEngines();
+
+        }
+
+        void workEngines() {
+            System.out.println("engines are working now");
+        }
+
+    }
+    class Bus extends Vehicule {
+
+    }
+
+
+
+
+
+
+    
+
